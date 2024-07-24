78b8ff19e350b752a132da902b3185f6a2965909
MateuSh5453 <mateush5453@gmail.com>
2024-07-23 22:45:34 -0300

    Finalizei o exercício

---
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,12 @@
+package horadosistema;
+
+import java.util.Date;
+
+public class HoraDoSistema {
+    public static void main(String[] args) throws Exception {
+      
+        Date data = new Date();
+        System.out.print("A data atual do sistema é: ");
+        System.out.println(data.toString());
+    }
+}
