7d6d2d18598a8d39d15c7f855d30d32f5fa21da4
MateuSh5453 <mateush5453@gmail.com>
2024-08-02 01:42:10 -0300

    Finalizeio a tabuada

--- IdiomaSwing/src/main/java/com/mycompany/idiomaswing/IdiomaSwing.java
+++ IdiomaSwing/src/main/java/com/mycompany/idiomaswing/IdiomaSwing.java
@@ -74,7 +74,7 @@ public class IdiomaSwing extends javax.swing.JFrame {
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         Locale idioma = Locale.getDefault();
-        idiomaSistema.setText(idioma.getDisplayLanguage()));
+        idiomaSistema.setText(idioma.getDisplayLanguage());
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
---
+++ Tabuada/src/Tabuda.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Tabuda {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um numero para ver a tabuada: ");
+        int n1 = scanner.nextInt();
+
+        int c = 1;
+        int resultado;
+        while (c <= 10) {
+            resultado = n1 * c; 
+            System.out.println(n1 +" x " + c + "= " + resultado);
+            c++;
+        }
+
+    }
+}
