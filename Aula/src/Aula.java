bcd04416cc9de89a43916c65deb4aaad2dd9b653
MateuSh5453 <mateush5453@gmail.com>
2024-08-06 02:06:17 -0300

    atualizei o repositorio

---
+++ Aula/src/Aula.java
@@ -0,0 +1,7 @@
+public class Aula {
+    public static void main(String[] args) {
+        float nota = 1.8f;
+        System.out.print("Sua nota é" + nota);
+    }
+    
+}
--- Tabuada/src/Tabuda.java
+++ Tabuada/src/Tabuda.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Tabuda {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Digite um numero para ver a tabuada: ");
