e639160d32ac53d298d74064ef185f6fa984579f
Laboratório FIAP <labsfiap@fiap.com.br>
2024-08-21 16:46:47 -0300

    o programa estava com poblema no nextLine porem conseguir resolver

--- ParOuImpar/src/ParOuImpar.java
+++ ParOuImpar/src/ParOuImpar.java
@@ -3,19 +3,31 @@ import java.util.Scanner;
 public class ParOuImpar {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        char fechar = 'N';
+        char resposta = 'N';
 
-        System.out.print("Digite um numero:");
-        int numero = entrada.nextInt();
+        while (true) {
+            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+            System.out.print("Digite um numero:");
+            int numero = entrada.nextInt();
 
-        if (numero % 2 == 0) {
+            if (numero % 2 == 0) {
+                System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                System.out.println("O numero digitado é PAR!");
+            } else {
+                System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+                System.out.println("O numero digitado é IMPAR!");
+            }
+
             System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-            System.out.println("O numero digitado é PAR!");
-        }else {
-            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-            System.out.println("O numero digitado é IMPAR!");
+            System.out.println("Deseja verificar outro numero? [S/N]");
+            resposta = entrada.next().charAt(0);
+            if (resposta == fechar) {
+                break;
+            }
         }
         System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-        System.out.println("Deseja verificar outro numero? [S/N]");
-
+        System.out.println("PROGRAMA FINALIZADO!!!");
+        entrada.close();
     }
 }
