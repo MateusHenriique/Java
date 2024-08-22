27bb75f908311150bdb798bada233ca8e10dd199
Mateus Henrique Da Silva <mateush5453@gmail.com>
2024-08-22 17:02:57 -0300

    finalizei

---
+++ AumentoSalario/src/AumentoSalario.java
@@ -0,0 +1,9 @@
+public class AumentoSalario {
+    public static void main(String[] args) {
+
+        int resultado = 1;
+        int Resultado = 2;
+        System.out.println(Resultado);
+    }
+
+}
