efbc8583236022bc259cb4d21d91cda19d6aa65a
MateuSh5453 <mateush5453@gmail.com>
2024-08-22 23:48:02 -0300

    Finalizei

--- AumentoSalario/src/AumentoSalario.java
+++ AumentoSalario/src/AumentoSalario.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class AumentoSalario {
     public static void main(String[] args) {
-
-        int resultado = 1;
-        int Resultado = 2;
-        System.out.println(Resultado);
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Qual e o salario de funcionario: ");
+        float salario = entrada.nextFloat();
+        float aumento_Salario = salario + ((salario/100)*15);
+        System.out.printf("O salario do funcionario com um aumento de 15%% foi de R$%.2f para R$%.2f", salario, aumento_Salario);
+        entrada.close();
     }
 
 }
