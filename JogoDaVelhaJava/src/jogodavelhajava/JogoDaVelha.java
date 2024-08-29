63ff0d8ccb0f8bd9191934d03b6be22c08722517
Mateus Henrique Da Silva <mateush5453@gmail.com>
2024-08-29 17:56:32 -0300

    o jogo esta com falha na atribuição

--- JogoDaVelhaJava/src/jogodavelhajava/JogoDaVelha.java
+++ JogoDaVelhaJava/src/jogodavelhajava/JogoDaVelha.java
@@ -6,34 +6,44 @@ public class JogoDaVelha {
 	public static void main(String[] args) {
 		
 		String matriz[][] = new String[3][3];
-		matriz = criaJogo(matriz);
-		System.out.print(matriz[0][0]);
+		String jogada = "0";
 		
 		Scanner input = new Scanner(System.in);
 
 		System.out.println("escolha com qual voce quer jogar: [X / O]");
 		char xouO = input.nextLine().charAt(0);
+		System.out.println(xouO);
 		
-		String jogada = " ";
-		escreveJogo(matriz, jogada, xouO);
-		System.out.println("\nEm qual posição voce deseja jogar:");
+		criaJogo(matriz, jogada, xouO);
+		System.out.println("\nEm qual posicao voce deseja jogar:");
 		jogada = input.nextLine();
-		escreveJogo(matriz, jogada, xouO);
+		criaJogo(matriz, jogada, xouO);
 
 		
 		input.close();
 	}
 	
-	public static String[][] criaJogo(String matrizfunc[][]) {
+	public static String[][] criaJogo(String matrizFunc[][], String jogada, char xouO) {
 		
+		String xouO2  = Character.toString(xouO);
 		int cont = 1;
 		
 		for (int c = 0; c < 3; c++) {
 			
-			for (int c2 = 0; c2 < 3; c2++) {
+			System.out.println();
+			for (int c2 = 0; c2 < 3; c2++) {	
 				
-				matrizfunc[c][c2] = Integer.toString(cont);
-				cont++;
+				if (matrizFunc[c][c2] == jogada) {
+						
+					matrizFunc[c][c2] = xouO2;
+					System.out.printf(" %s", matrizFunc[c][c2]);
+					cont++;
+					
+				}else {
+					matrizFunc[c][c2] = Integer.toString(cont);
+					System.out.printf(" %s", matrizFunc[c][c2]);
+					cont++;
+				}
 				
 			}
 			
@@ -41,25 +51,7 @@ public class JogoDaVelha {
 		
 		
 		}
-		return matrizfunc;
+		return matrizFunc;
 	}
 	
-	public static void escreveJogo(String matrizproced[][],String jogada, char xouO) {
-		
-		for (int c = 0; c < 3; c++) {
-			
-			System.out.println();
-			for (int c2 = 0; c2 < 3; c2++) {
-				
-				if (matrizproced[c][c2] == jogada) {
-
-					matrizproced[c][c2] = "X";
-					System.out.printf(" %s", matrizproced[c][c2]);
-				}else {
-					System.out.printf(" %s", matrizproced[c][c2]);
-				}
-			}
-		}
-		
-	}
 }
