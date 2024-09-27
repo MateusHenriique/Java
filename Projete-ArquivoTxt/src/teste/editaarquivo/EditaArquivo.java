233b3908f34eed1c9153c3fffae4461ac7a413c9
MateusHenriique <Mateush5453@gmail.com>
2024-09-26 23:01:24 -0300

    Finalizei o programa

--- Projete-ArquivoTxt/src/teste/editaarquivo/EditaArquivo.java
+++ Projete-ArquivoTxt/src/teste/editaarquivo/EditaArquivo.java
@@ -1,15 +1,15 @@
-package teste.cria;
+package teste.editaarquivo;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Cria {
-	private String caminho;
+public class EditaArquivo {
+	private String caminho = "S:\\DEV\\Workspaces\\Eclipse\\Java\\Projete-ArquivoTxt\\src";
 	
-	public void criaArquivo(String caminho) throws IOException {
-		this.caminho = caminho;
+	public void criaArquivo(String nome) throws IOException {
+		this.caminho = nome;
 		FileWriter escritor = new FileWriter(getCaminho());
 		escritor.write("Este e um arquivo criado em por um codigo java");
 		escritor.close();
@@ -17,7 +17,7 @@ public class Cria {
 	
 	public void escreveArquivo(String conteudo) throws IOException {
 		FileWriter escritor = new FileWriter(getCaminho(), true);
-		escritor.write("\n" + "\n " + conteudo);
+		escritor.write("\n" + conteudo);
 		escritor.close();
 	}
 	
@@ -29,6 +29,11 @@ public class Cria {
 			System.out.println(linha);
 		}
 		buffer.close();
+	}
+	
+	public void deletaDadosArquivo() throws IOException {
+		FileWriter deleta = new FileWriter(getCaminho());
+		deleta.close();
 	}
 	
 	public String getCaminho() {
---
+++ Projete-ArquivoTxt/src/teste/principal/Teste.java
@@ -0,0 +1,66 @@
+package teste.principal;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import teste.editaarquivo.EditaArquivo;
+
+public class Teste {
+
+	public static void main(String[] args) throws IOException {
+		EditaArquivo editaArquivo = new EditaArquivo();
+		Scanner scannerInt = new Scanner(System.in);
+		Scanner scannerStr = new Scanner(System.in);
+		int operacao = 0;
+		
+		System.out.println("-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+		System.out.println("Qual o nome do arquivo que voce deseja criar: ");
+		String nomeArquivo = scannerStr.nextLine();
+		editaArquivo.criaArquivo("\\" + nomeArquivo + ".txt");
+		
+		while (operacao != 4) {
+			System.out.println("""
+					-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+					Qual operação voce deseja realizar: 
+					[1] Escrever no arquivo.
+					[2] Ler Arquivo
+					[3] Apagar dados do arquivo.
+					[4] Sair do programa
+					""");
+			operacao = scannerInt.nextInt();
+			
+			switch(operacao) {
+
+				case 1:
+					System.out.println("-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+					System.out.println("digite o conteudo que voce deseja adicionar no seu arquivo: [ (fim) para finalizar]");
+					String conteudo = scannerStr.nextLine();
+					while (!conteudo.equals("fim")) {
+						conteudo = scannerStr.nextLine();
+						editaArquivo.escreveArquivo(conteudo);
+					}
+					break;
+				case 2:
+					editaArquivo.lerArquivo();
+					break;
+				case 3:
+					System.out.println("-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+					editaArquivo.deletaDadosArquivo();
+					System.out.println("Dados deletados com sucesso.");
+					break;
+				case 4:
+					System.out.println("-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+					System.out.println("Finalizando programa...");
+					break;
+				default:
+					System.out.println("-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+					System.out.println("Operação invalida, tente novamente...");
+			}
+		}
+		scannerInt.close();
+		scannerStr.close();
+		System.out.println("-=-=-=-==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+		System.out.println("Programa finalizado!");
+
+	}
+}
